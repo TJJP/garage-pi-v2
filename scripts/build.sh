de7d1ea56e0f4f06dcd65a81542343d9ae1ce59b
TJJP <61469514+TJJP@users.noreply.github.com>
2022-08-14 02:01:32 -0400

    Remove old port 7118

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,7 +15,7 @@ build (){
 }
 
 setup (){
-	sudo docker run -v /etc/timezone:/etc/timezone --restart=always --device=/dev/mem:/dev/mem --name=garage-pi --privileged --publish 7119:7119 --publish 7118:7118 -d tjjp/garage-pi-v3
+	sudo docker run -v /etc/timezone:/etc/timezone --restart=always --device=/dev/mem:/dev/mem --name=garage-pi --privileged --publish 7119:7119 -d tjjp/garage-pi-v3
 	sudo crontab -l 2>/dev/null; echo -e  "0 2 * * 3 sudo docker exec -t garage-pi /code/scripts/update.sh && sudo docker container restart garage-pi >/dev/null 2>&1" | sudo crontab -
 	sudo docker container restart garage-pi
 }
