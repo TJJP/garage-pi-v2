415fa2c15299e2fe716cbcb00986608c323f51b1
TJJP <61469514+TJJP@users.noreply.github.com>
2022-08-14 01:42:47 -0400

    Update install file and docker file to run install.sh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-# arm32v7/node:11-slim is used because hypriot/rpi-node is deprecated
-# Node v11 is used because rpio is not yet working with node v12
-# Slim is used to keep the size of the image as small as possible
+# arm32v6/node:14-alpine is used because hypriot/rpi-node is deprecated
+# Alpine is used to keep the size of the image as small as possible
+# arm32v6 is used so it works on raspberry pi zero
 FROM arm32v6/node:14-alpine
 
 # This is where the garage-pi code will be placed
@@ -11,21 +11,7 @@ WORKDIR /code
 COPY . /code
 
 # Install
-RUN apk update
-RUN apk add sudo
-RUN apk add curl
-RUN sudo ln -snf /usr/share/zoneinfo/$(curl https://ipapi.co/timezone) /etc/localtime
-RUN sudo apk add tzdata
-RUN sudo apk add nano
-# RUN sudo apk add --no-cache cron
-RUN sudo apk add certbot
-RUN sudo apk add py3-setuptools
-RUN sudo apk add build-base
-RUN sudo npm install rpio
-RUN sudo apk add git
-RUN sudo npm install
-RUN sudo npm audit fix
-RUN sudo npx webpack --config webpack.config.js
-
+RUN apk add --update bash
+RUN sh ./scripts/install.sh
 
 CMD ["npm", "start"]
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,40 +1,36 @@
-#!/bin/bash
-#Script runs in docker container
-#start at root of code
+#!/bin/sh
+# Script runs in docker container
+# Start at root of code
 cd /code
-# refreshing the repositories
+
+# update repositories
 apk update
 
 # install sudo
 apk add sudo
 
 # Time settings
-apk add curl -f
+apk add curl
 sudo ln -snf /usr/share/zoneinfo/$(curl https://ipapi.co/timezone) /etc/localtime
-sudo apk add tzdata -f
+sudo apk add tzdata
 
 # install nano for crontab editing
-sudo apk add nano -f
-
-#Install certbot and cron
-sudo apk add cron -f
-sudo apk add certbot -f
+sudo apk add nano
 
 # Python and pip
 # This is the biggest part and takes a long time...
 # Needed to install node-gyp which is needed for rpio
 # Without rpio this project doesn't work
-sudo apk add python-setuptools -f
-sudo apk add build-essential -f
-sudo npm install make
+sudo apk add py3-setuptools
+sudo apk add build-base
 sudo npm install rpio
 
-# git used for updating
-sudo apk add git -f
+# git is used for updating
+sudo apk add git
 
-# Installs all modules in package.json and checks for security issues and fix them
+# Installs all modules in package.json and checks for security issues and fixes them
+sudo npx browserslist@latest --update-db
 sudo npm install
 sudo npm audit fix
-
-# Run webpack so that the bundle.js can be created
+sudo npm fund
 sudo npx webpack --config webpack.config.js
